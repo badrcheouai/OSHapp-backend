d259e05edb87ad1452860280cc8e27dbda265e85
huma <badrecheouai@hotmail.com>
2025-07-04 14:28:04 +0100

    Origin 1

---
+++ src/main/java/com/ohse/OSHapp/OsHappApplication.java
@@ -0,0 +1,13 @@
+package com.ohse.OSHapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsHappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsHappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ohse/OSHapp/OsHappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ohse.OSHapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsHappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
